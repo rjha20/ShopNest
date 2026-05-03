d89798b9537f9a81c0de2de76e32d21f1cd8abc7
rjha20 <rjha20731@gmail.com>
2026-05-03 19:12:27 +0530

    changes for v2

--- inngest/function.js
+++ inngest/function.js
@@ -46,8 +46,7 @@ export const syncUserDeletion = inngest.createFunction(
 
 //Inngest Function to delete coupon on expiry 
 export const deleteCouponOnExpiry = inngest.createFunction(
-    { id: 'delete-coupon-on-expiry' },
-    { event: 'app/coupon.expired' },
+    { id: 'delete-coupon-on-expiry', triggers: { event: 'app/coupon.expired' } },
     async ({ event, step }) => {
         const { data } = event
         const expiryDate = new Date(data.expires_at)
